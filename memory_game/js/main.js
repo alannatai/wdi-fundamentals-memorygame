b2e4f681db3ace7a8287c5f8fcd6b7cf7817e5f9
Alanna Tai <alanna.tai@hotmail.com>
2019-11-02 19:56:22 -0400

    create game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardThree);
+console.log("User flipped " + cardFour);
