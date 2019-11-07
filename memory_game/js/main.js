1d879b2f2a84b28a6833e5ee371ed0e1abbbca30
Alanna Tai <alanna.tai@hotmail.com>
2019-11-07 16:42:25 -0500

    Add matched cards alert logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,18 @@
+var cards = ["queen", "queen", "king", "king"]
+var cardsInPlay = [];
 var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cardTwo = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardThree);
-console.log("User flipped " + cardFour);
+cardsInPlay.push(cardOne)
+cardsInPlay.push(cardTwo)
+
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+    } else {
+        alert("Sorry, try again.")
+    }
+}
+
+console.log(`User flipped ${cardsInPlay[0]}`);
+console.log(`User flipped ${cardsInPlay[1]}`);
