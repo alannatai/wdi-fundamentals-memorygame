e2f08ad1d9a5f5733ec55a2557223c69e0a1e59e
Alanna Tai <alanna.tai@hotmail.com>
2019-11-09 18:51:12 -0500

    Convert card array into object

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,24 @@
-var cards = ["queen", "queen", "king", "king"]
+var cards = [{
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+];
 var cardsInPlay = [];
 
 var checkForMatch = () => {
@@ -12,9 +32,11 @@ var checkForMatch = () => {
 };
 
 var flipCard = cardId => {
-    console.log(`User flipped ${cards[cardId]}.`)
-    cardsInPlay.push(cards[cardId]);
+    console.log(`User flipped ${cards[cardId].rank}.`)
+    cardsInPlay.push(cards[cardId].rank);
     checkForMatch();
+    console.log(cards[cardId].cardImage)
+    console.log(cards[cardId].suit)
 };
 
 flipCard(0);
