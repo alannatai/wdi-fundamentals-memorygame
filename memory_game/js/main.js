da2d00b93477c27b3ed67ebbf2a1ca624f150a66
Alanna Tai <alanna.tai@hotmail.com>
2019-11-11 16:12:16 -0500

    Remove button cover, replace with onclick disable instead

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,61 +23,53 @@ var cards = [{
 var cardsInPlay = [];
 var gameScore = 1;
 var outcomeMsg = document.getElementById('outcome-message');
+var cardImg = document.getElementsByTagName('img');
 
 var createBoard = function() {
     for (var i = 0; i < cards.length; i++) {
         var cardElement = document.createElement('img');
         cardElement.setAttribute('src', 'images/back.png');
         cardElement.setAttribute('data-id', i);
-        cardElement.addEventListener('click', flipCard);
+        //add onclick that cannot be clicked twice
+        cardElement.setAttribute('onclick', 'flipCard(this); this.onclick=null;');
         document.getElementById('game-board').appendChild(cardElement);
     }
 };
 
-
-
 var checkForMatch = function() {
     if (cardsInPlay.length === 2) {
         var score = document.getElementById('score-num');
-        //add card cover to prevent flipping
-        var addCardCover = function() {
-            var cover = document.createElement('div');
-            cover.setAttribute('id', 'cover');
-            document.getElementById('cover-board').appendChild(cover);
-        };
 
         if (cardsInPlay[0] === cardsInPlay[1]) {
             outcomeMsg.textContent = "You found a match!";
+            //add point to score
             score.textContent = gameScore++;
-            addCardCover();
-
         } else {
             outcomeMsg.textContent = "Sorry, try again.";
-            addCardCover();
+        }
+        //disable onclick after outcome message is displayed
+        for (var i = 0; i < cardImg.length; i++) {
+            cardImg[i].onclick = null;
         }
     }
 };
 
-var flipCard = function() {
-    var cardId = this.getAttribute('data-id');
+var flipCard = (card) => {
+    var cardId = card.getAttribute('data-id');
 
-    this.setAttribute('src', cards[cardId].cardImage)
+    card.setAttribute('src', cards[cardId].cardImage)
     console.log(`User flipped ${cards[cardId].rank}.`)
     cardsInPlay.push(cards[cardId].rank);
     checkForMatch();
 };
 
 var gameReset = function() {
-    var cardsReset = document.getElementsByTagName('img');
-
-    for (var i = 0; i < cardsReset.length; i++) {
-        cardsReset[i].setAttribute('src', 'images/back.png')
+    for (var i = 0; i < cardImg.length; i++) {
+        cardImg[i].setAttribute('src', 'images/back.png');
+        cardImg[i].setAttribute('onclick', 'flipCard(this); this.onclick=null;');
     }
     outcomeMsg.textContent = null;
     cardsInPlay = [];
-
-    //remove card cover to allow flipping
-    document.getElementById('cover-board').removeChild(cover);
 };
 
 createBoard();
